25f83f84214699f9b87cd8181d8f99804916434b
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2023-07-01 07:00:41 +0100

    Added initial templates and configured them to respective routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import url_for, render_template
 from app import app
 
 
 @app.route('/')
 def home():
-    return 'Task Keeper'
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
