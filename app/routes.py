3acd9207f5591909a4052e1ba922922792174250
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2023-07-02 18:10:16 +0100

    Implemented registration functionalities and added route for the tasks page

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import url_for, render_template, flash, redirect
+from flask import url_for, render_template, flash, redirect, request
 from app.forms import RegistrationForm, LoginForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
-from app import app
+from werkzeug.urls import url_parse
+from app import app, db
 
 
 @app.route('/')
@@ -27,13 +28,32 @@ def login():
             return redirect(url_for('login'))
         # if user gets validated
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('home'))
+        next_page = request.args.get('next') # Handles urls stored in the next from login_required
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
     return render_template('login.html', title='Sign in', form=form)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            flash('User already exist, please login')
+            return redirect(url_for('login'))
+        new = User(username=form.username.data, email=form.email.data)
+        new.set_password(form.password.data)
+        db.session.add(new)
+        db.session.commit()
+        flash('congratulations, you are now a registered user')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Sign Up', form=form)
+
+@app.route('/tasks')
+@login_required
+def tasks():
+    return "Tasks form and tasks goes here"
 
 @app.route('/logout')
 def logout():
